69f7d3630a17b1602b7313d4d0d36e02b93c98bb
deepaktringapps <deepakkumar.r@tringapps.com>
2020-12-31 19:24:51 +0530

    Style changes

--- src/OurProcess/Ourprocess.js
+++ src/OurProcess/Ourprocess.js
@@ -8,11 +8,19 @@ import Slide from 'react-reveal/Slide';
 import Flash from 'react-reveal/Flash';
 
 export default class Ourprocess extends Component {
+    constructor(props) {
+        super(props)
+        const width = window.innerWidth
+        let right = true
 
-    gotocontactus = () => {
-        return (
-            <div class="goto-contact-from-our-process"><a style={{ textDecoration: 'none', color: 'white' }} href="/contactus">{'>>'}</a></div>
-        )
+        if(width < 550) {
+            right = false
+        }
+
+        this.state = {
+          right: right
+        }
+       
     }
 
     render() {
@@ -20,66 +28,53 @@ export default class Ourprocess extends Component {
             <div class="our-process-container-with-text">
                 <div class="our-work-process-title">Our Work Process </div>
                 <ul class="our-process-list-container">
-                    <li >
-                        <Slide right>
-                            <div className="each-process-item">
-                                <div className="icon-container">
-                                    <div class="process-number">1</div>
-                                    <Flash  >
-                                        <Order style={{ width: '90px', height: '90px' }} className="our-process-icon-style" />
-                                    </Flash>
-                                    {this.gotocontactus()}
-                                </div>
-                                <div className="sub-process-title">Book Your Bulk Order</div>
-                            </div>
-                        </Slide>
-                    </li>
-                    <li >
-                        <Slide right delay = {100}>
-                            <div className="each-process-item">
-                                <div className="icon-container">
-                                    <div class="process-number">2</div>
-                                    <div className="our-process-icon-style" >
-                                        <Flash  >
-                                            <Packing style={{ width: '90px', height: '90px' }} className="our-process-icon-style" />
-                                        </Flash>
-                                    </div>
-                                    {this.gotocontactus()}
-                                </div>
-                                <div className="sub-process-title">Pack Your Items</div>
-                            </div>
-                        </Slide>
-                    </li>
-                    <li >
-                        <Slide right delay = {200}>
-                            <div className="each-process-item">
-                                <div className="icon-container">
-                                    <div class="process-number">3</div>
-                                    <Flash  >
-                                        <Transport style={{ width: '90px', height: '90px' }} className="our-process-icon-style" />
-                                    </Flash>
-                                    {this.gotocontactus()}
-                                </div>
-                                <div className="sub-process-title">Move Your Items</div>
-                            </div>
-                        </Slide>
-                    </li>
-                    <li >
-                        <Slide right delay = {300}>
-                            <div className="each-process-item">
-                                <div className="icon-container">
-                                    <div class="process-number">4</div>
-                                    <Flash  >
-                                        <Delivary style={{ width: '90px', height: '90px' }} className="our-process-icon-style" />
-                                    </Flash>
-                                    {this.gotocontactus()}
-                                </div>
-                                <div className="sub-process-title">Deliver Your Items</div>
-                            </div>
-                        </Slide>
-                    </li>
+                    {this.eachProcessSteps(1,0,'Book Your Bulk Order')}
+                    {this.eachProcessSteps(2,100,'Pack Your Items')}
+                    {this.eachProcessSteps(3,200,'Move Your Items')}
+                    {this.eachProcessSteps(4,300,'Deliver Your Items')}
                 </ul>
             </div>
+        )
+    }
+
+    eachProcessSteps = (step,delay,title) => {
+        return (
+            <li >
+                <Slide right={this.state.right} delay = {delay}>
+                    <div className="each-process-item">
+                        <div className="icon-container">
+                            <div class="process-number">{step}</div>
+                            <Flash>
+                                {this.getStepImage(step)}
+                            </Flash>
+                            {this.gotocontactus()}
+                        </div>
+                        <div className="sub-process-title">{title}</div>
+                    </div>
+                </Slide>
+            </li>
+        )
+    }
+
+    getStepImage = (step) => {
+        const style = { width: '90px', height: '90px' }
+        switch (step) {
+            case 1:
+                return <Order style={style} className="our-process-icon-style" />
+            case 2:
+                return <Packing style={style} className="our-process-icon-style" />
+            case 3:
+                return <Transport style={style} className="our-process-icon-style" />
+            case 4:
+                return <Delivary style={style} className="our-process-icon-style" />
+            default:
+                return null
+        }
+    }
+
+    gotocontactus = () => {
+        return (
+            <div class="goto-contact-from-our-process"><a style={{ textDecoration: 'none', color: 'white' }} href="/contactus">{'>>'}</a></div>
         )
     }
 }
